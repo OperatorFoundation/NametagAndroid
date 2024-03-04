f9867f128f2abede89286534403e9841bb00580a
bragelbytes <joebragel@gmail.com>
2024-03-04 15:48:56 -0800

    update gradle and add yml file

--- TransmissionNametag/build.gradle.kts
+++ TransmissionNametag/build.gradle.kts
@@ -34,9 +34,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("com.github.operatorfoundation:KeychainAndroid:5.0.9")
     implementation("com.github.operatorfoundation:TransmissionAndroid:1.3.5")
     implementation(project(":Nametag"))
